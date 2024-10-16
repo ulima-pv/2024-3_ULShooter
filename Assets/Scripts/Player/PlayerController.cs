550bb12f4e4f03beb70ae1b79613d8f4ba4c5f7f
ProfL3402 <ProfL3402@lab.aca>
2024-10-15 21:55:57 -0500

    rotacion por camara y disparos

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
+    private Transform m_FirePoint;
+    [SerializeField]
+    private float m_FireRange = 10f;
+    [SerializeField]
     private CharacterController m_CharacterController;
     [SerializeField]
     private float m_Speed = 5.0f;
     [SerializeField]
     private float m_JumpHeight = 2.0f; 
+    [SerializeField]
+    private GameObject m_FireSphere;
 
     private Vector3 m_PlayerVelocity = Vector3.zero;
 
@@ -40,17 +46,53 @@ public class PlayerController : MonoBehaviour
             m_OnJump = false;
         }
 
+        // Muevo el objeto en referencia al forward de Camara
+        move = Camera.main.transform.forward * move.z + 
+            Camera.main.transform.right * move.x;
+        move.y = 0f;
+
+        // Rotacion
+        var angles = Quaternion.RotateTowards(
+            transform.rotation,
+            Camera.main.transform.rotation,
+            300f * Time.deltaTime
+        ).eulerAngles;
+        angles.x = 0f; // bloqueamos la rotacion
+        angles.z = 0f; // bloqueamos la rotacion
+
+        transform.rotation = Quaternion.Euler(angles);
+
         m_PlayerVelocity.y += Physics.gravity.y * Time.deltaTime;
         move = new Vector3(
             move.x * Time.deltaTime,
             m_PlayerVelocity.y * Time.deltaTime,
             move.z * Time.deltaTime
         );
+
         m_CharacterController.Move(move);
     }
 
     public void Jump()
     {
         m_OnJump = true;
+    }
+
+    public void Fire()
+    {
+        // 1. Lanzar un raycast
+        RaycastHit hit;
+        var collision = Physics.Raycast(
+            m_FirePoint.position,
+            Camera.main.transform.forward,
+            out hit,
+            m_FireRange
+        );
+        if (collision)
+        {
+            // 2. Donde colisione el raycast, ejecutar un sistema de particulas
+            Instantiate(m_FireSphere, hit.point, Quaternion.identity);
+        }
+
+        
     }
 }
--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,6 +12,7 @@ public class PlayerInput : MonoBehaviour
     private ULShooter m_ULShooterActions;
     private InputAction m_MovementAction;
     private InputAction m_JumpAction;
+    private InputAction m_FireAction;
 
     private void Awake()
     {
@@ -25,6 +27,15 @@ public class PlayerInput : MonoBehaviour
         m_JumpAction = m_ULShooterActions.Player.Jump;
         m_JumpAction.performed += DoJump;
         m_JumpAction.Enable();
+
+        m_FireAction = m_ULShooterActions.Player.Fire;
+        m_FireAction.performed += DoFire;
+        m_FireAction.Enable();
+    }
+
+    private void DoFire(InputAction.CallbackContext obj)
+    {
+        m_PlayerController.Fire();
     }
 
     private void DoJump(InputAction.CallbackContext obj)
